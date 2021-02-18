12c1cc96a9e258652ae7ba81de8609a6d28cb3da
Brandon Braun <brandonbraun653@gmail.com>
2021-02-18 06:59:04 -0600

    Pull up submodules

--- src/tasks/tsk_graphics.cpp
+++ src/tasks/tsk_graphics.cpp
@@ -18,14 +18,6 @@
 #include <src/tasks/tsk_common.hpp>
 #include <src/tasks/tsk_graphics.hpp>
 
-
-
-// TESTING ONLY
-#include <filesystem>
-#include <ChimeraSim/control>
-#include <ChimeraSim/transport>
-#include <Chimera/spi>
-
 namespace DC::Tasks::GFX
 {
   /*-------------------------------------------------------------------------------
@@ -41,20 +33,6 @@ namespace DC::Tasks::GFX
     -------------------------------------------------*/
     waitInit();
     Chimera::delayMilliseconds( 100 );
-
-    Chimera::SIM::setDriverType( Chimera::Peripheral::Type::PERIPH_SPI, Chimera::SIM::Driver_t::NETWORKED_CONTROL );
-
-    std::array<uint8_t, 10> txBuffer= { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    std::array<uint8_t, 10> rxBuffer;
-    rxBuffer.fill( 0 );
-
-
-    Chimera::SPI::DriverConfig cfg;
-    cfg.HWInit.hwChannel = Chimera::SPI::Channel::SPI1;
-
-    auto driver = Chimera::SPI::getDriver( Chimera::SPI::Channel::SPI1 );
-    driver->init( cfg );
-    driver->readWriteBytes( txBuffer.data(), rxBuffer.data(), rxBuffer.size() );
 
     size_t lastWoken;
     while ( true )
