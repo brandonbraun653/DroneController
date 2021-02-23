74553950c283870410db78d3e2c3cfa5ac4dd578
Brandon Braun <brandonbraun653@gmail.com>
2021-02-22 21:29:48 -0600

    Pull up submodules. Fix thread lock on tx

--- src/tasks/tsk_radio.cpp
+++ src/tasks/tsk_radio.cpp
@@ -40,6 +40,7 @@ namespace DC::Tasks::RADIO
   {
     using namespace Aurora::Logging;
     using namespace Chimera::Thread;
+    using namespace Ripple;
 
     /*-------------------------------------------------
     Wait to be told to initialize by monitor thread
@@ -58,10 +59,61 @@ namespace DC::Tasks::RADIO
     netintf->powerUp( network );
 
 
-    Ripple::boot( *network, *netintf );
+    size_t lastTx    = Chimera::millis();
+    size_t lastRx    = Chimera::millis();
+    bool transmitted = false;
+
+    std::array<uint8_t, 10> packet = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    /* Allocate a packet as if it were the real thing */
+    MsgFrag txMsg;
+    txMsg.fragmentData   = network->malloc( packet.size() );
+    txMsg.fragmentLength = packet.size();
+    txMsg.nextFragment   = nullptr;
+    txMsg.totalLength    = packet.size();
+
+    memcpy( txMsg.fragmentData, packet.data(), txMsg.fragmentLength );
+
+    MsgFrag rxMsg;
+    Chimera::Status_t result;
 
     while ( 1 )
     {
+      /*-------------------------------------------------
+      Periodically transmit data. Expects to talk with a
+      python program that will simply echo the data back.
+      -------------------------------------------------*/
+      if ( ( ( Chimera::millis() - lastTx ) > 1000 ) && !transmitted )
+      {
+        result = netintf->send( txMsg, 0 );
+
+        if ( result == Chimera::Status::READY )
+        {
+          lastTx      = Chimera::millis();
+          lastRx      = Chimera::millis();
+          transmitted = true;
+        }
+      }
+
+      /*-------------------------------------------------
+      Try and sample data. Expects to receive an echo of
+      the transmit.
+      -------------------------------------------------*/
+      if ( transmitted && ( ( Chimera::millis() - lastRx ) > 100 ) )
+      {
+        result = netintf->recv( rxMsg );
+        if ( ( result == Chimera::Status::READY ) && rxMsg.fragmentData && rxMsg.fragmentLength )
+        {
+          transmitted = false;
+          lastRx      = Chimera::millis();
+          getRootSink()->flog( Level::LVL_DEBUG, "Got data\r\n" );
+        }
+        else
+        {
+          getRootSink()->flog( Level::LVL_DEBUG, "Didn't receive data\r\n" );
+        }
+      }
+
       // Process the data pipes here?
       BKGD::kickDog( PrjTaskId::RADIO );
       Chimera::delayMilliseconds( 10 );
