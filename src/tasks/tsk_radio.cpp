f6d580bf2aaba6f4f31e1e653a1115f8ec717c91
Brandon Braun <brandonbraun653@gmail.com>
2021-06-19 10:25:46 -0500

    Submodule update

--- src/tasks/tsk_radio.cpp
+++ src/tasks/tsk_radio.cpp
@@ -137,11 +137,11 @@ namespace DC::Tasks::RADIO
     /*-------------------------------------------------
     Create two sockets for a full duplex pipe
     -------------------------------------------------*/
-    Socket_rPtr txSocket = context->socket( SocketType::PUSH, 256 );
+    Socket_rPtr txSocket = context->socket( SocketType::PUSH, 512 );
     txSocket->open( LOCAL_HOST_PORT );
     txSocket->connect( LOCAL_HOST_IP, LOCAL_HOST_PORT );
 
-    Socket_rPtr rxSocket = context->socket( SocketType::PULL, 256 );
+    Socket_rPtr rxSocket = context->socket( SocketType::PULL, 512 );
     rxSocket->open( LOCAL_HOST_PORT );
     rxSocket->connect( LOCAL_HOST_IP, LOCAL_HOST_PORT );
 
